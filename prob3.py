a0ba4803463e899b14f0d8a2a8845ba0f22dd3b0
Cloud User <ec2-user@ip-172-31-1-49.ap-south-1.compute.internal>
2019-06-10 04:18:48 +0000

    thank you

---
+++ prob3.py
@@ -0,0 +1,16 @@
+#!/user/bin/python3
+adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+x=[]
+y=[]
+a=5
+b=3
+for i in adhoc:
+	if adhoc[i]>a:
+		x.append(adhoc[i])
+for i in adhoc:
+	if adhoc[i]<3:
+		y.append(adhoc[])
+print(x)
+print(y)
+
+		
